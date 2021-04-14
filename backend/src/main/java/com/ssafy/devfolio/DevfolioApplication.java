50856dae740f39f21c0fbd95e68e938767e157f1
geonhee ryu <sadog94@naver.com>
2021-04-14 11:10:17 +0900

    [feat/BE] : 프로젝트 세팅
    Resolves : #resolve S04P31B104-12

---
+++ backend/src/main/java/com/ssafy/devfolio/DevfolioApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.devfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevfolioApplication.class, args);
+    }
+
+}
