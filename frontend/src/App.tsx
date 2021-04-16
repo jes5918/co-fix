0e50d347feafe4931f9dd0ab96b997dabf448cb7
jes5918 <jes5918@koreatech.ac.kr>
2021-04-17 01:30:50 +0900

    [feat/FE] : Frontend 구조 및 prettier and eslint

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './views/Home';
+
+function App() {
+  return (
+    <>
+      <div className='App'>안녕지누</div>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/views/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layer from '../containers/Layer';
+
+interface Props {}
+
+function Home(props: Props) {
+  // const {} = props;
+
+  return (
+    <>
+      <div>여긴 홈</div>
+      <Layer />
+    </>
+  );
+}
+
+export default Home;
