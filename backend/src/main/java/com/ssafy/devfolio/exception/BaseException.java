9f600732f37d9a98a18871d4e9b54fd3b84f1755
김민혁 <kminheuk@gmail.com>
2021-04-20 01:11:47 +0900

    [feat/BE] : ExceptionAdvice 구현 (WIP)
    ErrorCode 규칙 설정 및 HttpsStatus 논의 필요

---
+++ backend/src/main/java/com/ssafy/devfolio/exception/BaseException.java
@@ -0,0 +1,19 @@
+package com.ssafy.devfolio.exception;
+
+public class BaseException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
