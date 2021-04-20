a1de85c71220b158a5dc4518ab55dc63a580aa0f
명도균 <dorosee97@gmail.com>
2021-04-20 22:32:24 +0900

    [feat/FE] : Card 컴포넌트 제작
    - 토글 axios, 및 함수 미작성
    - props 인자 확인
    - 기본 프레임 및 아이콘 props style 설정 완료
    
    Resolves : #resolve S04P31B104-65 #resolve S04P31B104-67 #resolve S04P31B104-68

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import styled from 'styled-components';
-interface cardProps {}
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+interface cardProps extends styleCardProps {
+  thumbnailURL: string;
+  card: {
+    zzim: boolean;
+    pk: number;
+    madeby: string;
+    createdAt: string;
+    tags: Array<string>;
+  };
+  onHandleZZim: () => void;
+  onClickTag: () => void;
+}
+
+interface styleCardProps {
+  propsWidth?: number;
+  propsHeight?: number;
+  propsFontSize?: number;
+}
 const cardStyle = {
-  mainFrame: styled.div`
-    width: 300px;
-    height: 400px;
+  mainFrame: styled.div<styleCardProps>`
+    width: ${({ propsWidth }) => `${propsWidth ? propsWidth : 300}px`};
+    height: ${({ propsHeight }) => `${propsHeight ? propsHeight : 400}px`};
     overflow: hidden;
     position: relative;
     border-radius: 30px;
@@ -14,21 +32,24 @@ const cardStyle = {
       top: 0px;
       opacity: 1;
     }
+    &:hover {
+      box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
+    }
   `,
   imgFrame: styled.img`
-    width: 300px;
-    height: 400px;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
     z-index: 1;
     border-radius: 30px;
   `,
   hoverContainer: styled.div`
-    width: 300px;
-    height: 400px;
+    width: 100%;
+    height: 100%;
     z-index: 3;
     background-color: rgba(0, 0, 0, 0.55);
     position: absolute;
-    top: 200px;
+    top: 20%;
     opacity: 0;
     transition: all 0.5s cubic-bezier(0, 0, 0, 1);
     display: flex;
@@ -37,13 +58,45 @@ const cardStyle = {
   `,
   infoBox: styled.div`
     width: 100%;
-    height: 70%;
-
+    height: 75%;
     margin: 0;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 10%;
+  `,
+  madeby: styled.div<styleCardProps>`
+    color: white;
+    font-weight: bold;
+    font-size: ${({ propsFontSize }) =>
+      `${propsFontSize ? propsFontSize : 17}px`};
+    margin-bottom: 15px;
+    text-align: center;
+    word-break: keep-all;
+    line-height: ${({ propsFontSize }) =>
+      `${propsFontSize ? propsFontSize + 10 : 27}px`};
+  `,
+  zzimedIcon: styled(AiFillStar)`
+    position: absolute;
+    bottom: 5%;
+    right: 5%;
+    cursor: pointer;
+    color: rgb(215 164 4);
+    font-size: 30px;
+  `,
+  zzimIcon: styled(AiOutlineStar)`
+    position: absolute;
+    bottom: 5%;
+    right: 5%;
+    cursor: pointer;
+    color: rgb(215 164 4);
+    font-size: 30px;
   `,
   tagBox: styled.div`
     width: 100%;
-    height: 30%;
+    height: 25%;
     margin: 0;
     display: flex;
     flex-direction: column;
@@ -52,30 +105,71 @@ const cardStyle = {
     background-color: rgba(256, 256, 256, 0.85);
   `,
   tags: styled.div`
-    width: 85%;
-    font-size: 17px;
+    width: 80%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
+    word-break: keep-all;
+  `,
+  tag: styled.span<styleCardProps>`
+    font-size: ${({ propsFontSize }) =>
+      `${propsFontSize ? propsFontSize : 17}px`};
     color: #262626;
     font-weight: bold;
     text-align: center;
     font-family: 'NotoSans';
+    line-height: 20px;
+    word-break: keep-all;
+    margin-right: 5px;
   `,
 };
 
 function Card(props: cardProps) {
-  const {} = props;
+  const {
+    thumbnailURL,
+    propsWidth,
+    propsHeight,
+    propsFontSize,
+    card,
+    onHandleZZim,
+    onClickTag,
+  } = props;
 
   return (
-    <cardStyle.mainFrame>
-      <cardStyle.imgFrame
-        src="https://i0.wp.com/themes.svn.wordpress.org/elegant-portfolio/1.0.4/screenshot.png"
-        alt=""
-      />
+    <cardStyle.mainFrame propsWidth={propsWidth} propsHeight={propsHeight}>
+      <cardStyle.imgFrame src={thumbnailURL} alt="" />
       <cardStyle.hoverContainer>
         {/* hover */}
-        <cardStyle.infoBox></cardStyle.infoBox>
+        <cardStyle.infoBox>
+          <cardStyle.madeby propsFontSize={propsFontSize}>
+            {card && card.madeby}님의 템플릿
+          </cardStyle.madeby>
+          <cardStyle.madeby propsFontSize={propsFontSize}>
+            {card && card.createdAt}
+          </cardStyle.madeby>
+          {card && card.zzim ? (
+            <cardStyle.zzimedIcon onClick={() => onHandleZZim()} />
+          ) : (
+            <cardStyle.zzimIcon onClick={() => onHandleZZim()} />
+          )}
+        </cardStyle.infoBox>
         <cardStyle.tagBox>
           <cardStyle.tags>
-            #FE개발자 #React #TypeScript #카카오 #CardComponent
+            {card &&
+              card.tags.map(
+                (tag: string, i: number): ReactElement => {
+                  return (
+                    <cardStyle.tag
+                      key={i}
+                      onClick={() => onClickTag()}
+                      propsFontSize={propsFontSize}
+                    >
+                      {tag}
+                    </cardStyle.tag>
+                  );
+                },
+              )}
           </cardStyle.tags>
         </cardStyle.tagBox>
       </cardStyle.hoverContainer>
