0f4bf9099fb4bdb0aae4060da6c7903cfa087f9f
김민혁 <kminheuk@gmail.com>
2021-04-21 12:18:29 +0900

    [feat/BE] : 스웨거 관련 설정 추가
    Resolves : #resolve S04P31B104-115

--- backend/src/main/java/com/ssafy/devfolio/response/dto/BaseResponse.java
+++ backend/src/main/java/com/ssafy/devfolio/response/dto/BaseResponse.java
@@ -1,5 +1,7 @@
 package com.ssafy.devfolio.response.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,14 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(value = "기본 응답 포맷")
 public class BaseResponse {
+    @ApiModelProperty(value = "요청 성공 여부 (true / false)")
     private boolean success;
+    @ApiModelProperty(value = "Http Status (OK, NOT FOUND, ...)")
     private HttpStatus status;
+    @ApiModelProperty(value = "응답에 대한 상세 코드")
     private String code;
+    @ApiModelProperty(value = "응답에 대한 설명")
     private String message;
 }
