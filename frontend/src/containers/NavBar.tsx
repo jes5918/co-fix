9d081d60b217ad8edd030b9678279aa5ab444c18
jes5918 <jes5918@koreatech.ac.kr>
2021-04-22 11:38:01 +0900

    [feat/FE] : navbar 기본 디자인 init

--- frontend/src/containers/NavBar.tsx
+++ frontend/src/containers/NavBar.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavbarMainWrapper = styled.div`
+  position: sticky;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 50px;
+  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+`;
+
+const NavbarLogo = styled.div`
+  width: 100px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  background-color: #ff9500;
+`;
+
+const NavbarMenuWraper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-width: 20vw;
+`;
+
+const NavbarItem = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: auto 10px;
+
+  a {
+    padding: 2px;
+    font-size: 18px;
+    border-bottom: 3.5px solid transparent;
+    transition: all 0.3s ease-in-out;
+  }
+
+  a:hover,
+  a:focus {
+    border-bottom: 3.5px solid #ff9500;
+  }
+`;
 
 interface Props {}
 
@@ -7,19 +53,20 @@ function NavBar(props: Props) {
   const {} = props;
 
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
+    <NavbarMainWrapper>
+      <NavbarLogo>로고자리</NavbarLogo>
+      <NavbarMenuWraper>
+        <NavbarItem>
+          <Link to="/">Social Login</Link>
+        </NavbarItem>
+        <NavbarItem>
           <Link to="/testArea">TestArea</Link>
-        </li>
-        <li>
+        </NavbarItem>
+        <NavbarItem>
           <Link to="/Toggle">Toggle</Link>
-        </li>
-      </ul>
-    </>
+        </NavbarItem>
+      </NavbarMenuWraper>
+    </NavbarMainWrapper>
   );
 }
 
