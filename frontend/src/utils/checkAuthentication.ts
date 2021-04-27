b0f8bdead31633a4ed62852ead4478019c544190
jes5918 <jes5918@koreatech.ac.kr>
2021-04-28 01:04:29 +0900

    [feat/FE] : Login 상태에 따라 navbar 변경
    Resolves : #resolve S04P22B105-

--- frontend/src/utils/checkAuthentication.ts
+++ frontend/src/utils/checkAuthentication.ts
@@ -1,13 +1,8 @@
-import { logoutUser, getUserData } from '../modules/actions/userActions';
-import store from '../modules/store';
-import axios from 'axios';
-import { SET_AUTHENTICATED } from '../modules/types';
-
 export const CheckAuthentication = () => {
-  const userToken = localStorage.token;
-  if (userToken) {
-    store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common['Authorization'] = userToken;
-    store.dispatch(getUserData());
+  const user = localStorage.getItem('user');
+  if (user) {
+    return true;
+  } else {
+    return false;
   }
 };
