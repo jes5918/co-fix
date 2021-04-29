36f2dd35f485e4270ff933e85b12ab624c1d4b64
geonhee ryu <sadog94@naver.com>
2021-04-29 16:21:06 +0900

    [feat/BE] : 배포 서버 시간Asia/Seoul 변경
    Resolves : #resolve S04P31B104-163

--- backend/src/main/java/com/ssafy/devfolio/DevfolioApplication.java
+++ backend/src/main/java/com/ssafy/devfolio/DevfolioApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
@@ -12,6 +16,12 @@ public class DevfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevfolioApplication.class, args);
+        System.out.println("LocalDateTime.now() = " + LocalDateTime.now());
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
