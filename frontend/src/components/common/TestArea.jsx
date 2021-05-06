17f07bc7e058a931c7c2bfe9d7c8396caceadc28
유진우 <phoenix9373@naver.com>
2021-05-06 15:54:23 +0900

    [refactor/FE] : 이전 프로젝트 잔재 삭제
    Resolves : #resolve

--- frontend/src/components/common/TestArea.jsx
+++ frontend/src/components/common/TestArea.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Components
-import EditorMenu from '../../containers/editor/EditorMenu';
-import SideBar from '../dnd/SideBar';
-import Container from '../dnd/Container';
-import DropZone from '../dnd/DropZone';
-
-// React DND
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
 const Styled = {
   Space: styled.div`
     width: 100%;
@@ -18,37 +8,10 @@ const Styled = {
     display: flex;
     flex-direction: row;
   `,
-  WorkSpace: styled.div`
-    width: 80%;
-    height: 100%;
-    background-color: lightgreen;
-    display: flex;
-  `,
-  RightBar: styled.div`
-    width: 20%;
-    height: 100%;
-    background-color: lightskyblue;
-  `,
 };
 
 const TestArea = () => {
-  const onSubmit = (searchWord) => {
-    console.log('searchWord : ', searchWord);
-  };
-  return (
-    <Styled.Space>
-      <EditorMenu />
-      <DndProvider backend={HTML5Backend}>
-        <Styled.WorkSpace>
-          <SideBar>
-            <Container></Container>
-          </SideBar>
-          <DropZone></DropZone>
-        </Styled.WorkSpace>
-      </DndProvider>
-      <Styled.RightBar></Styled.RightBar>
-    </Styled.Space>
-  );
+  return <Styled.Space></Styled.Space>;
 };
 
 export default TestArea;
