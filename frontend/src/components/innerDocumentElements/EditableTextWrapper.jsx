13f3a0c966ff22c07a3cec8c62437054a5fbc54b
유진우 <phoenix9373@naver.com>
2021-05-07 10:43:07 +0900

    [feat/FE] : 컴포넌트 구조 세팅
    Resolves : #resolve

---
+++ frontend/src/components/innerDocumentElements/EditableTextWrapper.jsx
@@ -0,0 +1,17 @@
+// Roll : Document 컨테이너에서 각 문장을 감싸는 UI
+
+import React from 'react';
+
+const PositiveButton = () => {
+  return <button></button>;
+};
+
+const NegativeButton = () => {
+  return <button></button>;
+};
+
+function EditableTextWrapper(props) {
+  return <div></div>;
+}
+
+export default EditableTextWrapper;
---
+++ frontend/src/containers/CommentContainer.jsx
@@ -0,0 +1,9 @@
+// Roll : 편집 화면에서 우측 Comment 작성 및 보이는 부분의 상태를 관리함.
+
+import React from 'react';
+
+function CommentContainer(props) {
+  return <div></div>;
+}
+
+export default CommentContainer;
