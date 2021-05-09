7b42a608fd547ed440c92b48e16732136c59f871
명도균 <dorosee97@gmail.com>
2021-05-09 23:06:54 +0900

    [feat/FE] : Home,Create Frame
    - 기본 프레임

---
+++ frontend/src/containers/home/Section_2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = {
+  wrapper: styled.div`
+    width: 100%;
+    height: 50vh;
+    background-color: #fcf7ef;
+  `,
+};
+export default function Section_2() {
+  return <Section.wrapper></Section.wrapper>;
+}
