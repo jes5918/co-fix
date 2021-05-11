1e12bb3bfe14ad20a3f16c56fb02c3456a54dcef
김민혁 <kminheuk@gmail.com>
2021-05-12 02:25:22 +0900

    [fix/BE] : Swagger 적용
    Resolves : #resolve S04P31B104-224

--- backend/src/main/java/com/ssafy/devfolio/sentence/SentenceController.java
+++ backend/src/main/java/com/ssafy/devfolio/sentence/SentenceController.java
@@ -5,8 +5,7 @@ import com.ssafy.devfolio.response.ResponseService;
 import com.ssafy.devfolio.response.dto.BaseResponse;
 import com.ssafy.devfolio.response.dto.ListDataResponse;
 import com.ssafy.devfolio.sentence.dto.SentenceFixRequest;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +19,15 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/documents")
+@Api(value = "문장 api")
 public class SentenceController {
 
     private final SentenceService sentenceService;
     private final ResponseService responseService;
 
+    @ApiOperation(value = "문서 조회", notes = "문서 id를 이용해 문서 내부 문장 전체 조회")
     @GetMapping("/{documentId}")
-    public ResponseEntity getDocument(@PathVariable String documentId) {
+    public ResponseEntity getDocument(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId) {
         List<Sentence> document = sentenceService.getDocument(documentId);
 
         ListDataResponse<Sentence> response = responseService.getListDataResponse(document, HttpStatus.OK);
@@ -37,11 +38,12 @@ public class SentenceController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 발급받는 Bearer token", required = true, dataType = "String", paramType = "header")
     })
+    @ApiOperation(value = "문장 수정", notes = "첨삭방 개설자만 문장 수정 가능")
     @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/{documentId}/sentences/{sentenceId}")
-    public ResponseEntity fixSentence(@PathVariable String documentId,
-                                      @PathVariable String sentenceId,
-                                      @RequestBody SentenceFixRequest request) throws JsonProcessingException {
+    public ResponseEntity fixSentence(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+                                      @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
+                                      @ApiParam(value = "문장 수정 정보", required = true) @RequestBody SentenceFixRequest request) throws JsonProcessingException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         long memberId = Long.parseLong(authentication.getName());
 
