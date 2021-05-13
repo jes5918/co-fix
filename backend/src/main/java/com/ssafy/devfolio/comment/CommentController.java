3384ebc4c02ad54de21c789e7fa95faac5fd70cc
김민혁 <kminheuk@gmail.com>
2021-05-13 12:10:40 +0900

    [feat/BE] : 문장에 달린 코멘트 조회 api 구현
    Resolves : #resolve S04P31B104-239

--- backend/src/main/java/com/ssafy/devfolio/comment/CommentController.java
+++ backend/src/main/java/com/ssafy/devfolio/comment/CommentController.java
@@ -3,6 +3,7 @@ package com.ssafy.devfolio.comment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.devfolio.comment.dto.CommentRequest;
 import com.ssafy.devfolio.response.ResponseService;
+import com.ssafy.devfolio.response.dto.ListDataResponse;
 import com.ssafy.devfolio.response.dto.SingleDataResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,12 +29,21 @@ public class CommentController {
     public ResponseEntity writeComment(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                        @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
                                        @ApiParam(value = "코멘트 작성 정보", required = true) @RequestBody CommentRequest request) throws JsonProcessingException {
-        System.out.println(documentId);
-        System.out.println(sentenceId);
 
         Comment comment = commentService.writeComment(documentId, sentenceId, request);
 
         SingleDataResponse<Comment> response = responseService.getSingleDataResponse(comment, HttpStatus.CREATED);
+
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ApiOperation(value = "문장에 달린 코멘트 조회")
+    @GetMapping("/sentences/{sentenceId}/comments")
+    public ResponseEntity getComments(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+                                       @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId) throws JsonProcessingException {
+        List<Comment> comments = commentService.getComments(sentenceId);
+
+        ListDataResponse<Comment> response = responseService.getListDataResponse(comments, HttpStatus.OK);
 
         return ResponseEntity.status(response.getStatus()).body(response);
     }
