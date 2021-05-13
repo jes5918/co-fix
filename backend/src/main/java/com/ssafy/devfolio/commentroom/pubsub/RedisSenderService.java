0f0254e365828b063de57447649d105a3629e8d7
geonhee ryu <sadog94@naver.com>
2021-05-13 16:41:33 +0900

    [fix/BE] - 매퍼 삭제, onMessage 예외로그

--- backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSenderService.java
+++ backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSenderService.java
@@ -6,38 +6,24 @@ import com.ssafy.devfolio.commentroom.CommentRoom;
 import com.ssafy.devfolio.commentroom.dto.CommentRoomSub;
 import com.ssafy.devfolio.sentence.Sentence;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-
 @Service
 @RequiredArgsConstructor
 public class RedisSenderService {
 
     private final String COMMENT_ROOM_PREFIX = "room:";
-    private final String DOCUMENT_PREFIX = "document:";
-    private final String MEMBER_ROOM_PREFIX = "member-room:";
 
     private final RedisTemplate<String, String> redisTemplate;
-    private HashOperations<String, String, String> hashOperations;
-    private ValueOperations<String, String> valueOperations;
-    private ListOperations<String, String> listOperations;
+    private final ValueOperations<String, String> valueOperations;
 
     private final ChannelTopic channelTopic;
     private final ObjectMapper objectMapper;
 
-    @PostConstruct
-    public void init() {
-        valueOperations = redisTemplate.opsForValue();
-        hashOperations = redisTemplate.opsForHash();
-        listOperations = redisTemplate.opsForList();
-    }
     private final SimpMessageSendingOperations messageTemplate;
 
     public void sendRoomUpdateService(CommentRoom room) throws JsonProcessingException {
--- backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSubscriber.java
+++ backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSubscriber.java
@@ -3,6 +3,7 @@ package com.ssafy.devfolio.commentroom.pubsub;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.devfolio.commentroom.dto.CommentRoomSub;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,6 +11,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RedisSubscriber implements MessageListener {
 
@@ -25,6 +27,7 @@ public class RedisSubscriber implements MessageListener {
             messageTemplate.convertAndSend("/room/" + commentRoomSub.getRoomId(), commentRoomSub);
 
         } catch (Exception e) {
+            log.error(e.getMessage());
         }
     }
 }
