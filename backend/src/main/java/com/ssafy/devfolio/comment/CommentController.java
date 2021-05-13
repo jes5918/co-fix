59fa4b227aa4a1bae01c7259621e936363201796
김민혁 <kminheuk@gmail.com>
2021-05-13 13:44:38 +0900

    [fix/BE] : 코멘트, 문장 api uri에 첨삭방 id 포함하도록 수정

--- backend/src/main/java/com/ssafy/devfolio/comment/CommentController.java
+++ backend/src/main/java/com/ssafy/devfolio/comment/CommentController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/documents/{documentId}")
+@RequestMapping("/commentRooms/{commentRoomId}/documents/{documentId}")
 @Api(value = "코멘트 api. 경로에 문서 id 필수")
 public class CommentController {
 
@@ -29,7 +29,8 @@ public class CommentController {
 
     @ApiOperation(value = "문장에 코멘트 작성")
     @PostMapping("/sentences/{sentenceId}/comments")
-    public ResponseEntity writeComment(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+    public ResponseEntity writeComment(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                       @ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                        @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
                                        @ApiParam(value = "코멘트 작성 정보", required = true) @RequestBody CommentRequest request) throws JsonProcessingException {
 
@@ -42,7 +43,8 @@ public class CommentController {
 
     @ApiOperation(value = "문장에 달린 코멘트 조회")
     @GetMapping("/sentences/{sentenceId}/comments")
-    public ResponseEntity getComments(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+    public ResponseEntity getComments(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                      @ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                       @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId) throws JsonProcessingException {
         List<Comment> comments = commentService.getComments(sentenceId);
 
@@ -53,7 +55,8 @@ public class CommentController {
 
     @ApiOperation(value = "코멘트에 agree (이미 한 사람이 요청시 취소)")
     @PostMapping("/sentences/{sentenceId}/comments/{commentId}/agree")
-    public ResponseEntity getComments(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+    public ResponseEntity getComments(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                      @ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                       @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
                                       @ApiParam(value = "코멘트 id", required = true) @PathVariable String commentId,
                                       @ApiParam(value = "작성자 닉네임", required = true) @RequestParam(required = true, defaultValue = "") String nickname) throws JsonProcessingException {
