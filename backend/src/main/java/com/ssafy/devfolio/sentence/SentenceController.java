87c9d02daa40d3938e625c7f38054ef65bd665b1
김민혁 <kminheuk@gmail.com>
2021-05-13 13:44:38 +0900

    [fix/BE] : 코멘트, 문장 api uri에 첨삭방 id 포함하도록 수정

--- backend/src/main/java/com/ssafy/devfolio/sentence/SentenceController.java
+++ backend/src/main/java/com/ssafy/devfolio/sentence/SentenceController.java
@@ -19,8 +19,8 @@ import static com.ssafy.devfolio.utils.Utility.getMemberIdFromAuthentication;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/documents")
-@Api(value = "문장 api")
+@RequestMapping("/commentRooms/{commentRoomId}/documents")
+@Api(value = "문장 api. 방id 필수")
 public class SentenceController {
 
     private final SentenceService sentenceService;
@@ -28,7 +28,8 @@ public class SentenceController {
 
     @ApiOperation(value = "문서 조회", notes = "문서 id를 이용해 문서 내부 문장 전체 조회")
     @GetMapping("/{documentId}")
-    public ResponseEntity getDocument(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId) {
+    public ResponseEntity getDocument(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                      @ApiParam(value = "문서 id", required = true) @PathVariable String documentId) {
         List<Sentence> document = sentenceService.getDocument(documentId);
 
         ListDataResponse<Sentence> response = responseService.getListDataResponse(document, HttpStatus.OK);
@@ -42,7 +43,8 @@ public class SentenceController {
     @ApiOperation(value = "문장 수정", notes = "첨삭방 개설자만 문장 수정 가능")
     @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/{documentId}/sentences/{sentenceId}")
-    public ResponseEntity fixSentence(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+    public ResponseEntity fixSentence(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                      @ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                       @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
                                       @ApiParam(value = "문장 수정 정보", required = true) @RequestBody SentenceFixRequest request) throws JsonProcessingException {
         Long memberId = getMemberIdFromAuthentication();
@@ -56,7 +58,8 @@ public class SentenceController {
 
     @ApiOperation(value = "문장 감정표현 - 테스트용 (소켓 연결 후 제거 예정)", notes = "이미 감정표현 한 사람이 또 하면 취소")
     @PostMapping("/{documentId}/sentences/{sentenceId}/feelings")
-    public ResponseEntity pressFeeling(@ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
+    public ResponseEntity pressFeeling(@ApiParam(value = "첨삭방 id", required = true) @PathVariable String commentRoomId,
+                                       @ApiParam(value = "문서 id", required = true) @PathVariable String documentId,
                                        @ApiParam(value = "문장 id", required = true) @PathVariable String sentenceId,
                                        @ApiParam(value = "감정표현 정보", required = true) @RequestBody FeelingRequest request) throws JsonProcessingException {
         sentenceService.pressFeeling(documentId, sentenceId, request);
