20cca2d84392d5852709cea9a1187cd76cda7d10
geonhee ryu <sadog94@naver.com>
2021-05-14 14:11:18 +0900

    [feat/BE] : 소켓/레디스 - 문장 코멘트
    Resolves : #resolve S04P31B104-246

--- backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSenderService.java
+++ backend/src/main/java/com/ssafy/devfolio/commentroom/pubsub/RedisSenderService.java
@@ -2,10 +2,13 @@ package com.ssafy.devfolio.commentroom.pubsub;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.devfolio.comment.Comment;
+import com.ssafy.devfolio.comment.dto.SentenceSub;
 import com.ssafy.devfolio.commentroom.CommentRoom;
 import com.ssafy.devfolio.commentroom.dto.CommentRoomSub;
 import com.ssafy.devfolio.sentence.Sentence;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -17,16 +20,17 @@ import org.springframework.stereotype.Service;
 public class RedisSenderService {
 
     private final String COMMENT_ROOM_PREFIX = "room:";
+    private final String SENTENCE_PREFIX = "sentence:";
 
     private final RedisTemplate<String, String> redisTemplate;
     private final ValueOperations<String, String> valueOperations;
+    private final HashOperations<String, String, String> hashOperations;
 
-    private final ChannelTopic channelTopic;
     private final ObjectMapper objectMapper;
 
     private final SimpMessageSendingOperations messageTemplate;
 
-    public void sendRoomUpdateService(CommentRoom room) throws JsonProcessingException {
+    public void sendRoomUpdateService(ChannelTopic channelTopic, CommentRoom room) throws JsonProcessingException {
         CommentRoomSub commentRoomSub = CommentRoomSub.builder()
                 .roomId(room.getRoomId())
                 .roomTitle(room.getRoomTitle())
@@ -37,11 +41,11 @@ public class RedisSenderService {
                 .build();
 
         redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(commentRoomSub));
-        messageTemplate.convertAndSend("/room/" + commentRoomSub.getRoomId(), commentRoomSub);
+//        messageTemplate.convertAndSend("/room/" + commentRoomSub.getRoomId(), commentRoomSub);
 
     }
 
-    public void sendSentenceUpdateService(String roomId, Sentence sentence) throws JsonProcessingException {
+    public void sendSentenceUpdateService(ChannelTopic channelTopic, String roomId, Sentence sentence) throws JsonProcessingException {
         CommentRoom commentRoom = objectMapper.readValue(valueOperations.get(COMMENT_ROOM_PREFIX + roomId), CommentRoom.class);
 
         CommentRoomSub commentRoomSub = CommentRoomSub.builder()
@@ -50,7 +54,26 @@ public class RedisSenderService {
                 .lastModifiedDate(commentRoom.getLastModifiedDate())
                 .build();
 
+        System.out.println("channelTopic = " + channelTopic);
+        System.out.println("channelTopic.getTopic() = " + channelTopic.getTopic());
+
         redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(commentRoomSub));
-        messageTemplate.convertAndSend("/room/" + commentRoomSub.getRoomId(), commentRoomSub);
+//        messageTemplate.convertAndSend("/room/" + commentRoomSub.getRoomId(), commentRoomSub);
+    }
+
+    public void sendCommentUpdateService(ChannelTopic channelTopic, String sentenceId, Comment comment) throws JsonProcessingException {
+
+        SentenceSub sentenceSub = SentenceSub.builder()
+                .sentenceId(sentenceId)
+                .commentId(comment.getCommentId())
+                .nickname(comment.getNickname())
+                .content(comment.getContent())
+                .agree(comment.getAgree())
+                .lastModifiedDate(comment.getLastModifiedDate())
+                .build();
+
+        redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(sentenceSub));
+//        messageTemplate.convertAndSend("/sentence/" + sentenceId, sentenceSub);
+
     }
 }
--- backend/src/main/java/com/ssafy/devfolio/commentroom/service/CommentRoomService.java
+++ backend/src/main/java/com/ssafy/devfolio/commentroom/service/CommentRoomService.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.devfolio.commentroom.CommentRoom;
 import com.ssafy.devfolio.commentroom.RoomStatus;
 import com.ssafy.devfolio.commentroom.dto.CreateCommentRoomRequest;
-import com.ssafy.devfolio.commentroom.pubsub.RedisSubscriber;
+import com.ssafy.devfolio.commentroom.pubsub.RedisRoomSubscriber;
 import com.ssafy.devfolio.exception.BaseException;
 import com.ssafy.devfolio.exception.ErrorCode;
 import com.ssafy.devfolio.member.MemberRepository;
@@ -17,11 +17,9 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -49,8 +47,8 @@ public class CommentRoomService {
     private final ListOperations<String, String> listOperations;
 
     private final RedisMessageListenerContainer redisMessageListener;
-    private final RedisSubscriber redisSubscriber;
-    private final ChannelTopic channelTopic;
+    private final RedisRoomSubscriber redisRoomSubscriber;
+//    private final ChannelTopic channelTopic;
     private final ObjectMapper objectMapper;
     private final MemberRepository memberRepository;
 
@@ -249,25 +247,25 @@ public class CommentRoomService {
                 .map(wrapper(this::getCommentRoomById))
                 .collect(Collectors.toList());
     }
-
-    /**
-     * 소켓
-     * 첨삭방 전체 리턴하는 메서드
-     */
-    public void sendCommentRoom(CommentRoom commentRoom) throws JsonProcessingException {
-
-        redisMessageListener.addMessageListener(redisSubscriber, channelTopic);
-
-        redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(commentRoom));
-    }
-
-    /**
-     * 소켓
-     *  첨삭방 - 문장 감정표현 변화
-     */
-    public void sendCommentRoomFeeling(Sentence sentence) throws JsonProcessingException {
-        redisMessageListener.addMessageListener(redisSubscriber, channelTopic);
-
-        redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(sentence));
-    }
+//
+//    /**
+//     * 소켓
+//     * 첨삭방 전체 리턴하는 메서드
+//     */
+//    public void sendCommentRoom(CommentRoom commentRoom) throws JsonProcessingException {
+//
+//        redisMessageListener.addMessageListener(redisSubscriber, channelTopic);
+//
+//        redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(commentRoom));
+//    }
+//
+//    /**
+//     * 소켓
+//     *  첨삭방 - 문장 감정표현 변화
+//     */
+//    public void sendCommentRoomFeeling(Sentence sentence) throws JsonProcessingException {
+//        redisMessageListener.addMessageListener(redisSubscriber, channelTopic);
+//
+//        redisTemplate.convertAndSend(channelTopic.getTopic(), objectMapper.writeValueAsString(sentence));
+//    }
 }
