067bdfa0f5fdc192afa561de77a9595041f2f391
jes5918 <jes5918@koreatech.ac.kr>
2021-05-24 13:13:20 +0900

    [fix/FE] : 오타수정

--- frontend/src/containers/RoomSettingButtonContainer.jsx
+++ frontend/src/containers/RoomSettingButtonContainer.jsx
@@ -259,7 +259,7 @@ export default function RoomSettingButtonContainer({
           </B.RoomInfomationUsers>
         </B.RoomInfomationWrapper>
       </Modal>
-    </q>
+    </>
   );
 }
 const B = {
